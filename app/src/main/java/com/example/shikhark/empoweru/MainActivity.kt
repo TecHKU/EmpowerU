dc06a3db6e374110a71ab50a9193ca2f912bfc86
Shikhar Khandelwal <shikhark@Shikhars-MacBook-Pro.local>
2018-10-14 16:07:49 +0800

    Added Co-routines functionality

--- app/src/main/java/com/example/shikhark/empoweru/MainActivity.kt
+++ app/src/main/java/com/example/shikhark/empoweru/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.shikhark.empoweru
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        launch(UI) {
+            delay(5000)
+            toast("Hi Manvi")
+        }
+
+
     }
+
+
+
+
+}
+
+
+fun Activity.toast(content:String){
+    Toast.makeText(this,content,Toast.LENGTH_SHORT).show()
 }
